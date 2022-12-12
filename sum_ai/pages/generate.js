3672ddc846e88843a3380a62e104fa9d533149c5
Aiden Ramirez <51302686+ramirezra4@users.noreply.github.com>
2022-12-12 01:11:18 -0800

    Basic UI and Routing, pre-gpt

---
+++ sum_ai/pages/generate.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Generate = () => {
+  const [text, setText] = useState('');
+  const [display, setDisplay] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  }
+
+  const applyPlaceholder = (inputText) => {
+    // This is where you would apply the placeholder operation on the text
+    return inputText;
+  }
+
+  const handleGenerate = () => {
+    const placeholderText = applyPlaceholder(text);
+    setDisplay(placeholderText);
+  }
+
+  return (
+    <div>
+      <button onClick={handleGenerate}>Generate</button>
+      <input type="text" onChange={handleChange} />
+      <div>{display}</div>
+    </div>
+  );
+};
+export default Generate
---
+++ sum_ai/pages/learn.js
@@ -0,0 +1,5 @@
+function Learn() {
+    return <div>Learn</div>
+}
+
+export default Learn
